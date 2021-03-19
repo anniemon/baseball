a304ef457d3e579f2060d44a92a3d4f6bf3151ef
seojung94 <73627443+seojung94@users.noreply.github.com>
2021-03-19 13:23:08 +0900

    button event added, random number generator created

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello, vanilla.");
+const startBtn = document.querySelector("button");
+
+
+function generateNumber(){
+   let result = '';
+   for (var i=0; i<3; i++){
+       result += Math.floor(Math.random()*10);
+   }
+   console.log(result);
+}
+
+function init (){
+    startBtn.addEventListener("click", generateNumber);
+}
+
+init();
