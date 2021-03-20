704935e88a36579b2d862a21bb77e5f13b87a6ef
seojung94 <73627443+seojung94@users.noreply.github.com>
2021-03-20 23:56:29 +0900

    form input added
    if submitted number length is not 3
    show alert

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const startBtn = document.querySelector("button");
+const startBtn = document.querySelector("button"),
+    form = document.querySelector(".js-numberForm"),
+    input = form.querySelector("input");
 
 
 function generateNumber(){
@@ -6,10 +8,26 @@ function generateNumber(){
    for (var i=0; i<3; i++){
        result += Math.floor(Math.random()*10);
    }
-   console.log(result);
+   return result;
+}
+
+function handleSubmit (){
+    const currentValue = input.value;
+    if (currentValue.length !== 3){
+        alert("세 자릿수를 입력하세요.");
+    }
+    compareNumber(currentValue);
+}
+
+function compareNumber(){
+    const answer = generateNumber();
+    if (answer ===){
+
+    }
 }
 
 function init (){
+    form.addEventListener("submit", handleSubmit);
     startBtn.addEventListener("click", generateNumber);
 }
 
