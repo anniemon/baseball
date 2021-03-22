aa019803ea9a68dfa8ec1506446b502ec57ab6ab
seojung94 <73627443+seojung94@users.noreply.github.com>
2021-03-22 23:46:36 +0900

    compare submitted number and answer by strike

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const startBtn = document.querySelector("button"),
     form = document.querySelector(".js-numberForm"),
     input = form.querySelector("input");
 
+let result = '';
 
 function generateNumber(){
-   let result = '';
    for (var i=0; i<3; i++){
        result += Math.floor(Math.random()*10);
    }
-   return result;
+   console.log(result);
 }
 
 function handleSubmit (){
@@ -16,13 +16,31 @@ function handleSubmit (){
     if (currentValue.length !== 3){
         alert("세 자릿수를 입력하세요.");
     }
-    compareNumber(currentValue);
+    else {
+        compareNumber(currentValue);
+    }
 }
 
-function compareNumber(){
-    const answer = generateNumber();
-    if (answer ===){
-
+ function compareNumber(submittedNumber){
+    if (result.charAt(0) === submittedNumber.charAt(0)
+    && result.charAt(1) === submittedNumber.charAt(1)
+    && result.charAt(2) === submittedNumber.charAt(2)){
+        alert('3 strike');
+    }
+    else if (
+    (result.charAt(0) === submittedNumber.charAt(0)
+    && result.charAt(1) === submittedNumber.charAt(1))
+    || (result.charAt(0) === submittedNumber.charAt(0)
+    && result.charAt(2) === submittedNumber.charAt(2))
+    || (result.charAt(1) === submittedNumber.charAt(1)
+    && result.charAt(2) === submittedNumber.charAt(2))
+    ){
+        alert('2 strike');
+    }
+    else if((result.charAt(0) === submittedNumber.charAt(0)
+    || result.charAt(1) === submittedNumber.charAt(1)
+    || result.charAt(2) === submittedNumber.charAt(2))){
+        alert('1 strike');
     }
 }
 
